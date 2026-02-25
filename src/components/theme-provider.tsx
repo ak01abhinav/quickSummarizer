81d455f5a183600b2081980b731f6f520d604d18
Abhinav_ak <abhinav01.ak@gmail.com>
2026-02-25 17:33:12 +0530

    random comment committed to verify ci check2

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -3,6 +3,7 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 // comment
+// comment2
 export function ThemeProvider({
   children,
   ...props
